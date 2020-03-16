a74d2688f356316082fa1833081e132bfd41a6c6
Harrison <hmseaborn@gmail.com>
2020-03-16 12:17:02 -0700

    added an initial state to todoReducer

---
+++ todo/src/reducers/todoReducer.js
@@ -0,0 +1,19 @@
+export const initialState = {
+    todoList: [
+       {  
+          item: 'learn about reducers',
+          completed: false,
+          id: Date.now()
+        },
+        {  
+          item: 'learn about reducers some more',
+          completed: false,
+          id: Date.now()
+        },
+        {  
+          item: 'learn about reducers even more',
+          completed: false,
+          id: Date.now()
+        },
+   ]
+}
