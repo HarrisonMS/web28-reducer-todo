fef0e8dd001d8ea081c7e5be141f973df0e9c649
Harrison <hmseaborn@gmail.com>
2020-03-16 13:11:47 -0700

    added toggleCompleted case
    map through array toggle completed item by payload

--- todo/src/reducers/todoReducer.js
+++ todo/src/reducers/todoReducer.js
@@ -29,6 +29,14 @@ export const todoReducer = (currentState, action) => {
         ...currentState,
         todoList: [...currentState.todoList, newTodo]
       }
+    case 'TOGGLE_COMPLETED':
+      return {
+        ...currentState,
+        todoList: currentState.todoList.map(todo => todo.id === action.payload ? {...todo, completed: !todo.completed } : todo)
+        // spreads in current state then maps over the todo lis array checking every todo by comparing if todo id is equal to the id of the todo attempting to toggle complete
+        // if true spread in the tdo with the key completed toggled with ! to opposite of whatever it was set to
+        // if the todo id does not equal the action.payload return todo
+      }
       default: return currentState;
   } 
 }
